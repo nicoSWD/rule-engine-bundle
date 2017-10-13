59088ad8aef35d28da3a45602c2a2654f3a3e42c
Nicolas Oelgart <nico@nicoswd.com>
2017-10-13 21:00:32 +0200

    Add tests

---
+++ tests/nicoSWD/RuleBundle/Model/RuleTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tests\nicoSWD\RuleBundle\Model;
+
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\Mock;
+use nicoSWD\RuleBundle\Model\Rule;
+use nicoSWD\Rules\Evaluator;
+use nicoSWD\Rules\Parser;
+
+class RuleTest extends \PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /** @var Parser|Mock */
+    private $parser;
+
+    /** @var Evaluator|Mock */
+    private $evaluator;
+
+    /** @var Rule */
+    private $rule;
+
+    protected function setUp()
+    {
+        $this->parser = \Mockery::mock(Parser::class)->shouldIgnoreMissing();
+        $this->evaluator = \Mockery::mock(Evaluator::class);
+
+        $this->rule = new Rule($this->parser, $this->evaluator);
+    }
+
+    public function testGivenARuleWhenTheEvaluatorReturnsTrueItShouldReturnTrue()
+    {
+        $this->parser->shouldReceive('parse')->once()->andReturn('1');
+        $this->evaluator->shouldReceive('evaluate')->once()->andReturn(true);
+
+        $this->assertTrue($this->rule->isTrue('1 === 1'));
+    }
+
+    public function testGivenARuleWhenTheEvaluatorReturnsFalseItShouldReturnFalse()
+    {
+        $this->parser->shouldReceive('parse')->once()->andReturn('1');
+        $this->evaluator->shouldReceive('evaluate')->once()->andReturn(false);
+
+        $this->assertFalse($this->rule->isTrue('1 === 2'));
+    }
+}
