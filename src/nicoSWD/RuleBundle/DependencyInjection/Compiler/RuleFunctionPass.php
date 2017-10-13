583ac6148f7b02c218fb8b1f4e3680c30e4effff
Nicolas Oelgart <nico@nicoswd.com>
2017-10-13 15:28:17 +0200

    Fix and remove some stuff

--- src/nicoSWD/RuleBundle/DependencyInjection/Compiler/RuleFunctionPass.php
+++ src/nicoSWD/RuleBundle/DependencyInjection/Compiler/RuleFunctionPass.php
@@ -9,19 +9,13 @@ class RuleFunctionPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if (!$container->has('nico_swd.rules.parser')) {
-            return;
-        }
+        if ($container->has('nico_swd.rules.parser')) {
+            $definition = $container->findDefinition('nico_swd.rules.parser');
+            $serviceIds = array_keys($container->findTaggedServiceIds('nico_swd.rule.function'));
 
-        /** @var \nicoSWD\Rules\Parser $parser */
-        $parser = $container->findDefinition('nico_swd.rules.parser');
-        $functions = $container->findTaggedServiceIds('nico_swd.rule.function');
-
-        foreach ($functions as $id => $function) {
-            /** \nicoSWD\Rules\Core\CallableFunction $function */
-            $parser->registerFunction($function->getName(), function () use ($function) {
-                return $function->call(...func_get_args());
-            });
+            foreach ($serviceIds as $serviceId) {
+                $definition->addMethodCall('registerFunctionClass', [get_class($container->get($serviceId))]);
+            }
         }
     }
 }
